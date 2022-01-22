15c8265400ebcfe011c7d458c3e2d26ac433d45b
Korin777 <57750932+Korin777@users.noreply.github.com>
2022-01-22 19:56:50 +0800

    use -O3 to optimization

--- toolchain.cmake
+++ toolchain.cmake
@@ -10,21 +10,9 @@ set(CMAKE_ASM_COMPILER ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_OBJCOPY ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_SIZE ${TOOLCHAIN_PREFIX}size)
 
-set(ARCH_FLAGS "-march=rv32i -mabi=ilp32")
+set(ARCH_FLAGS "-march=rv32i -mabi=ilp32 -O3")
 
 set(CMAKE_C_FLAGS "${ARCH_FLAGS} -std=gnu11 -Wall -ffunction-sections -fdata-sections")
 set(CMAKE_ASM_FLAGS "${ARCH_FLAGS} -x assembler-with-cpp")
 set(CMAKE_EXE_LINKER_FLAGS "-Wl,-Map=${CMAKE_CURRENT_BINARY_DIR}/output.map -Wl,--gc-sections -u _printf_float -u _scanf_float -lm")
 
-set(CMAKE_C_FLAGS_DEBUG "-Og -g")
-set(CMAKE_ASM_FLAGS_DEBUG "-g")
-
-set(CMAKE_C_FLAGS_RELEASE "-Os")
-set(CMAKE_ASM_FLAGS_RELEASE "")
-
-
-function(add_embedded_binary TARGET)
-	add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_OBJCOPY} -O binary ${TARGET} ${TARGET}.bin)
-	add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_OBJCOPY} -O ihex ${TARGET} ${TARGET}.hex)
-	add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_SIZE} ${TARGET})
-endfunction()
